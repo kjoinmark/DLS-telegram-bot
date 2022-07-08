96e036398bd148aaf90d2bb50560f4dbfe2aa141
Kate <kjoinmark@yandex.ru>
2022-07-08 18:20:43 +0700

    Update style_transfer_bot_final_version_demonstration.py

--- style_transfer_bot_final_version_demonstration.py
+++ style_transfer_bot_final_version_demonstration.py
@@ -11,41 +11,9 @@ import ssl
 from telebot.async_telebot import AsyncTeleBot
 import asyncio
 
-API_TOKEN = '5592278771:AAHjv-KggUzfHsBRpktcX5Lwd9K4VTnMJ4Q' # put here our telegram api token
-
-#WEBHOOK_HOST = '35.239.10.73'
-#WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
-#WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
-
-#WEBHOOK_SSL_CERT = '/home/matrs011/ssl_key/url_cert.pem'  # Path to the ssl certificate
-#WEBHOOK_SSL_PRIV = '/home/matrs011/ssl_key/url_private.key'  # Path to the ssl private key
-
-
-
-#WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
-#WEBHOOK_URL_PATH = "/{}/".format(API_TOKEN)
-
-#logger = telebot.logger
-#telebot.logger.setLevel(logging.INFO)
+API_TOKEN = 'TOKEN' # put here our telegram api token
 
 bot = AsyncTeleBot(API_TOKEN)
-#bot = telebot.TeleBot(API_TOKEN)
-#app = web.Application()
-
-
-# Process webhook calls
-#async def handle(request):
-#    if request.match_info.get('token') == bot.token:
-#        request_body_dict = await request.json()
-#        update = telebot.types.Update.de_json(request_body_dict)
-#        bot.process_new_updates([update])
-#        return web.Response()
-#    else:
-#        return web.Response(status=403)
-
-
-#app.router.add_post('/{token}/', handle)
-
 
 import tensorflow as tf
 import IPython.display as display
@@ -200,22 +168,3 @@ async def getting_photo(message):
             await bot.send_message(chat_id, "!!!something wrong with ind_variable!!!")
 print('start polling')
 asyncio.run(bot.infinity_polling())
-
-# Remove webhook, it fails sometimes the set if there is a previous webhook
-#bot.remove_webhook()
-
-# Set webhook
-#bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
-#                certificate=open(WEBHOOK_SSL_CERT, 'r'))
-
-# Build ssl context
-#context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-#context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
-
-# Start aiohttp server
-#web.run_app(
-#    app,
-#    host=WEBHOOK_LISTEN,
-#    port=WEBHOOK_PORT,
-#    ssl_context=context,
-#)
